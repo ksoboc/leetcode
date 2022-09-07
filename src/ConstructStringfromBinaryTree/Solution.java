157faefb3e6206e35eeb0326947f93f8e3d80848
krzysztof <krszysztof@yopmail.com>
2022-09-07 05:56:09 +0200

    New problem: Construct String from Binary Tree - imporved performance - only one stringbuilder globally

--- src/ConstructStringfromBinaryTree/Solution.java
+++ src/ConstructStringfromBinaryTree/Solution.java
@@ -2,17 +2,27 @@ package ConstructStringfromBinaryTree;
 
 public class Solution {
     public String tree2str(TreeNode root) {
-        if (root == null)
-            return "";
-        StringBuilder sb = new StringBuilder().append(root.val);
-        if (root.left != null || root.right != null)
-            sb.append("(").append(tree2str(root.left)).append(")");
-        if (root.right != null)
-            sb.append("(").append(tree2str(root.right)).append(")");
-
+        StringBuilder sb = new StringBuilder();
+        tree2str(root, sb);
         return sb.toString();
-
     }
+
+    private void tree2str(TreeNode root, StringBuilder sb) {
+        if (root == null)
+            return;
+        sb.append(root.val);
+        if (root.left != null || root.right != null) {
+            sb.append("(");
+            tree2str(root.left, sb);
+            sb.append(")");
+        }
+        if (root.right != null) {
+            sb.append("(");
+            tree2str(root.right,sb);
+            sb.append(")");
+        }
+    }
+
 }
 
 class TreeNode {
