b7af78b81f1a1296a46ea070d5d17214cd2529af
krzysztof <krszysztof@yopmail.com>
2022-12-09 08:16:50 +0100

    Maximum Difference Between Node and Ancestor second approach calculate max and min along the way and calculate diff at the leaf node

--- src/MaximumDifferenceBetweenNodeandAncestor/Solution.java
+++ src/MaximumDifferenceBetweenNodeandAncestor/Solution.java
@@ -4,20 +4,23 @@ import Common.TreeNode;
 
 public class Solution {
     public int maxAncestorDiff(TreeNode root) {
-        if (root == null)
-            return -1;
-        return dfs(root, root.val, root.val);
+        if (root == null) {
+            return 0;
+        }
+        return helper(root, root.val, root.val);
     }
 
-    private int dfs(TreeNode node, int min, int max) {
-        if (node == null)
-            return -1;
-
-        int val1 = Math.max(Math.abs(min - node.val), Math.abs(max - node.val));
-        min = Math.min(min, node.val);
-        max = Math.max(max, node.val);
-        int val2 = Math.max(dfs(node.left, min, max), dfs(node.right, min, max));
-
-        return Math.max(val1, val2);
+    public int helper(TreeNode node, int curMax, int curMin) {
+        // if encounter leaves, return the max-min along the path
+        if (node == null) {
+            return curMax - curMin;
+        }
+        // else, update max and min
+        // and return the max of left and right subtrees
+        curMax = Math.max(curMax, node.val);
+        curMin = Math.min(curMin, node.val);
+        int left = helper(node.left, curMax, curMin);
+        int right = helper(node.right, curMax, curMin);
+        return Math.max(left, right);
     }
 }
