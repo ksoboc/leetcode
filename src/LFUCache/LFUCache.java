0ae58357f0cd18906d43e854e38b06d6c4a46e94
krzysztof <krszysztof@yopmail.com>
2023-01-31 06:26:35 +0100

    new problems solved

---
+++ src/LFUCache/LFUCache.java
@@ -0,0 +1,85 @@
+package LFUCache;
+
+import java.util.*;
+
+public class LFUCache {
+    private Map<Integer, Pair<Integer, Integer>> cache;
+    private Map<Integer, LinkedHashSet<Integer>> frequencies;
+    private int minf;
+    private int capacity;
+
+    public LFUCache(int capacity) {
+        cache = new HashMap<>();
+        frequencies = new HashMap<>();
+        minf = 0;
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        Pair<Integer, Integer> frequencyAndValue = cache.get(key);
+        if (frequencyAndValue == null) {
+            return -1;
+        }
+        final int frequency = frequencyAndValue.getKey();
+        final Set<Integer> keys = frequencies.get(frequency);
+        keys.remove(key);
+        if (minf == frequency && keys.isEmpty()) {
+            ++minf;
+        }
+        final int value = frequencyAndValue.getValue();
+        insert(key, frequency + 1, value);
+        return value;
+    }
+
+    private void insert(int key, int frequency, int value) {
+        cache.put(key, new Pair<>(frequency, value));
+        frequencies.putIfAbsent(frequency, new LinkedHashSet<>());
+        frequencies.get(frequency).add(key);
+    }
+
+    public void put(int key, int value) {
+        if (capacity <= 0) {
+            return;
+        }
+        Pair<Integer, Integer> frequencyAndValue = cache.get(key);
+        if (frequencyAndValue != null) {
+            cache.put(key, new Pair<>(frequencyAndValue.getKey(), value));
+            get(key);
+            return;
+        }
+        if (capacity == cache.size()) {
+            final Set<Integer> keys = frequencies.get(minf);
+            final int keyToDelete = keys.iterator().next();
+            cache.remove(keyToDelete);
+            keys.remove(keyToDelete);
+        }
+        minf = 1;
+        insert(key, 1, value);
+    }
+}
+
+class Pair<K,V> {
+    K key;
+    V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+}
