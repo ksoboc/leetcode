a6ef99e514a64df322be92c3fb3312757e38820e
KS <ks@yopmail.com>
2023-02-21 08:43:23 +0100

    Inna prostsza wersja SingleElementinaSortedArray

--- src/SingleElementinaSortedArray/Solution.java
+++ src/SingleElementinaSortedArray/Solution.java
@@ -2,28 +2,18 @@ package SingleElementinaSortedArray;
 
 public class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int r= nums.length-1;
-        int l= 0;
-        while (l<=r) {
-            int mid = l + (r-l)/2;
-            boolean even = mid%2==0;
-            if (even) {
-                if (mid< nums.length-2 && nums[mid] == nums[mid + 1]) {
-                    l = mid + 2;
-                } else if (mid>1 && nums[mid] == nums[mid-1]) {
-                    r=mid;
-                } else
-                    return nums[mid];
-            } else {
-                if (mid< nums.length-1 && nums[mid] == nums[mid+1]) {
-                    r=mid-1;
-                } else  if (mid>0 && nums[mid] == nums[mid-1]) {
-                    l=mid+1;
-                } else
-                    return nums[mid];
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (mid % 2 == 1) {
+                mid--;
             }
-
+            if (nums[mid] != nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 2;
+            }
         }
-        return -1;
+        return nums[left];
     }
 }
