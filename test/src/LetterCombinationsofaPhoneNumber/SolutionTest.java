8a68017b76e54c0406d271beae9954f4b39e792e
krzysztof <rexel22@o2.pl>
2023-08-03 08:21:37 +0200

    New problems solved - Letter Combinations of a Phone Number

--- test/src/LetterCombinationsofaPhoneNumber/SolutionTest.java
+++ test/src/LetterCombinationsofaPhoneNumber/SolutionTest.java
@@ -2,6 +2,9 @@ package LetterCombinationsofaPhoneNumber;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
@@ -9,19 +12,19 @@ class SolutionTest {
     void test1() {
         Solution solution=new Solution();
         var res = solution.letterCombinations("23");
-        System.out.println("res = " + res);
+        assertEquals(List.of("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"),res);
     }
     @Test
     void test2() {
         Solution solution=new Solution();
         var res = solution.letterCombinations("");
-        System.out.println("res = " + res);
+        assertEquals(Collections.emptyList(),res);
     }
     @Test
     void test3() {
         Solution solution=new Solution();
         var res = solution.letterCombinations("2");
-        System.out.println("res = " + res);
+        assertEquals(List.of("a","b","c"),res);
     }
 
 }
